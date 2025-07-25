b699575fab382b2109c6da7a575929a99587dd28
Joseph <jo.liu@f5.com>
2025-07-25 12:19:27 -0700

    added tree sitter and renamed file

--- lua/plugins/!plugins.lua
+++
@@ -1,6 +0,0 @@
-return {
-    { 'neovim/nvim-lspconfig' },
-    { 'HiPhish/rainbow-delimiters.nvim' },
-    { "nvim-tree/nvim-web-devicons", opts = {} },
-    { "j-hui/fidget.nvim", opts = {} }
-}
---
+++ lua/plugins/_plugins.lua
@@ -0,0 +1,7 @@
+return {
+    { 'neovim/nvim-lspconfig' },
+    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+    { "nvim-tree/nvim-web-devicons", opts = {} },
+    { "j-hui/fidget.nvim", opts = {} },
+    { 'HiPhish/rainbow-delimiters.nvim' }
+}
