6a876de1a1368c32825b2bb0cbcb88bf64475ce5
Joseph <jo.liu@f5.com>
2025-07-25 19:22:38 +0000

    rainbow-delims submods disabled for update speed

--- lua/plugins/_plugins.lua
+++ lua/plugins/_plugins.lua
@@ -3,5 +3,5 @@ return {
     {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
     { "nvim-tree/nvim-web-devicons", opts = {} },
     { "j-hui/fidget.nvim", opts = {} },
-    { 'HiPhish/rainbow-delimiters.nvim' }
+    { 'HiPhish/rainbow-delimiters.nvim', submodules = false }
 }
