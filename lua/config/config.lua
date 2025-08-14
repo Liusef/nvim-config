22bf2638a5739cb2eb1edce2dc18643d14462fcc
Joseph Liu <>
2025-08-14 15:03:35 -0700

    added wsl clipboard config

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -16,3 +16,23 @@ vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
+
+-- WSL Clipboard config 
+if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+        name = "WSLClipboard",
+        copy = {
+            ["+"] = '/mnt/c/windows/system32/clip.exe',
+            ["*"] = '/mnt/c/windows/system32/clip.exe',
+        },
+        paste = {
+            ["+"] = (function()
+                return vim.fn.systemlist('/mnt/c/windows/system32/windowspowershell/v1.0/powershell.exe -NoLogo -NoProfile -c \'[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))\'', {''}, 1) -- '1' keeps empty lines
+            end),
+            ["*"] = (function()
+                return vim.fn.systemlist('/mnt/c/windows/system32/windowspowershell/v1.0/powershell.exe -NoLogo -NoProfile -c \'[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))\'', {''}, 1)
+            end),
+        },
+        cache_enabled = true
+    }
+end
